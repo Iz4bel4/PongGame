63ad29c68c36be4a7b8d9f23b8aff75a1586cff1
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-19 18:24:13 +0200

    Added functionality that detect collision with top and bottom wall, paddles and padles misses.  Added scoreboard. Made a ball move faster with each bounce of the ball, changed the way paddles move

--- ball.py
+++ ball.py
@@ -7,8 +7,24 @@ class Ball(Turtle):
         self.shape('circle')
         self.color('white')
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.ball_move_speed = 0.1
 
     def move_ball(self):
-        new_x_position = self.xcor() + 10
-        new_y_position = self.ycor() + 10
+        new_x_position = self.xcor() + self.x_move
+        new_y_position = self.ycor() + self.y_move
         self.goto(new_x_position, new_y_position)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.ball_move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.ball_move_speed = 0.1
+        self.bounce_x()
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 
@@ -19,17 +21,37 @@ ball = Ball()
 
 
 screen.listen()
-screen.onkey(right_paddle.go_up, 'Up')
-screen.onkey(right_paddle.go_down, 'Down')
+screen.onkeypress(right_paddle.go_up, 'Up')
+screen.onkeypress(right_paddle.go_down, 'Down')
 
-screen.onkey(left_paddle.go_up, 'w')
-screen.onkey(left_paddle.go_down, 's')
+screen.onkeypress(left_paddle.go_up, 'w')
+screen.onkeypress(left_paddle.go_down, 's')
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.ball_move_speed)
     ball.move_ball()
+
+    #Detect collision with top and bottom wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with paddles
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.right_point()
+
+    #Detect left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.left_point()
+
+
 
 screen.exitonclick()
